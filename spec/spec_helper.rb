71434f68bc0736059012a95c3e915d34c0781105
kunliu <kun.liu@dji.com>
2016-12-28 22:33:14 +0800

    clear database after spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-
 require 'smart_attr'
 require 'active_record'
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    File.delete(File.expand_path('../../db/smart_attr_test', __FILE__))
+  end
+end
